ca494804bb06a2c031099f17683b95ff27b9f644
rudrodip <official.rudrodipsarker@gmail.com>
2023-08-27 13:26:30 +0600

    removed contact button, and changed media query

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -7,10 +7,6 @@ import { getTableOfContents } from "@lib/toc";
 import { DashboardTableOfContents } from "@src/components/toc";
 import "@src/styles/mdx.css";
 import { Metadata } from "next";
-import Link from "next/link";
-import { cn } from "@lib/utils";
-import { buttonVariants } from "@src/components/ui/button";
-import { Contact } from "lucide-react";
 
 async function getpageFromParams(params: { slug: string }) {
   const slug = params.slug;
@@ -33,17 +29,7 @@ export default async function AboutPage() {
 
   return (
     <article className="container relative max-w-3xl py-6 lg:py-10">
-      <Link
-        href="/contact"
-        className={cn(
-          buttonVariants({ variant: "ghost" }),
-          "absolute left-[-200px] top-14 hidden xl:inline-flex"
-        )}
-      >
-        <Contact className="mr-2 h-4 w-4" />
-        Contact
-      </Link>
-      <div className="hidden text-sm min-[1690px]:inline-flex">
+      <div className="hidden text-sm min-[1790px]:inline-flex">
         <div className="fixed top-28 right-[100px] h-full z-50">
           <DashboardTableOfContents toc={toc} />
         </div>
