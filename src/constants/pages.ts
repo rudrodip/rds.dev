1e7cf9c58baa756fc6e6a3966e40249836fa2206
rudrodip <official.rudrodipsarker@gmail.com>
2023-08-27 15:18:04 +0600

    added pages.ts

---
+++ src/constants/pages.ts
@@ -0,0 +1,29 @@
+type PageType = {
+  name: string,
+  url: string
+}
+
+const pages: PageType[] = [
+  {
+    name: '/rudrodip',
+    url: '/'
+  },
+  {
+    name: '/about',
+    url: '/about'
+  },
+  {
+    name: '/projects',
+    url: '/projects'
+  },
+  {
+    name: '/blog',
+    url: '/blog'
+  },
+  {
+    name: '/contact',
+    url: '/#contact'
+  },
+]
+
+export { pages }
