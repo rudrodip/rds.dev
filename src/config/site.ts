1135c95dbcdc50ce982b1fac4ca94df38a096215
rudrodip <77154365+rudrodip@users.noreply.github.com>
2023-10-13 02:37:08 +0000

    added site config

---
+++ src/config/site.ts
@@ -0,0 +1,16 @@
+import { SiteConfig } from "@src/types"
+
+export const siteConfig: SiteConfig = {
+  name: "Rudrodip Sarker",
+  description: "I am Rudrodip Sarker, a student at Rajshahi College and enthusiast computer programmer building webapps, mobile apps, backend, python scripts for automation, embedded systems programming, robotics, and visualizing mathematical concepts using programming.",
+  url: "https://rudrodip.vercel.app",
+  ogImage: "https://rudrodip.vercel.app/og.png",
+  links: {
+    twitter: "https://twitter.com/rds_agi",
+    github: "https://github.com/rudrodip",
+    fb: "https://facebook.com/enthusiast.math",
+    insta: "https://instagram.com/rds_agi",
+    discord: "https://discordapp.com/users/841126921886498817",
+    youtube: "https://www.youtube.com/@rudrodipsarker",
+  },
+}
