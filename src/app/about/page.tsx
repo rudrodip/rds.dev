a7dbb9d23013fdd4bd6a58c0e2039b24fb488726
rudrodip <77154365+rudrodip@users.noreply.github.com>
2023-10-13 02:36:14 +0000

    improved seo

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -7,6 +7,13 @@ import { getTableOfContents } from "@src/lib/toc";
 import { DashboardTableOfContents } from "@src/components/toc";
 import "@src/styles/mdx.css";
 import { Metadata } from "next";
+import { siteConfig } from "@src/config/site";
+
+type AboutPageProps = {
+  params: {
+    slug: string;
+  };
+};
 
 async function getpageFromParams(params: { slug: string }) {
   const slug = params.slug;
@@ -16,6 +23,51 @@ async function getpageFromParams(params: { slug: string }) {
     null;
   }
   return page;
+}
+
+export async function generateMetadata({
+  params,
+}: AboutPageProps): Promise<Metadata> {
+  const page = await getpageFromParams({ slug: 'about' });
+
+  if (!page) {
+    return {};
+  }
+
+  const url = siteConfig.url;
+
+  const ogUrl = new URL(`${url}/assets/bg/aboutme.png`);
+  ogUrl.searchParams.set("heading", page.title);
+  ogUrl.searchParams.set("type", "website");
+  ogUrl.searchParams.set("mode", "dark");
+
+  return {
+    title: page.title,
+    description: page.description,
+    authors: [{
+      name: siteConfig.name
+    }],
+    openGraph: {
+      title: page.title,
+      description: page.description,
+      type: "website",
+      url: `${url}/about`,
+      images: [
+        {
+          url: ogUrl.toString(),
+          width: 1200,
+          height: 630,
+          alt: page.title,
+        },
+      ],
+    },
+    twitter: {
+      card: "summary_large_image",
+      title: page.title,
+      description: page.description,
+      images: [ogUrl.toString()],
+    },
+  };
 }
 
 export default async function AboutPage() {
