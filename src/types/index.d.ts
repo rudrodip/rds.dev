d92653bb661f9eb79cc76f23edd6fc5e301d9b7e
rudrodip <77154365+rudrodip@users.noreply.github.com>
2023-10-13 02:37:37 +0000

    added types

---
+++ src/types/index.d.ts
@@ -0,0 +1,14 @@
+export type SiteConfig = {
+    name: string,
+    description: string,
+    url: string,
+    ogImage: string,
+    links: {
+        twitter: string,
+        github: string,
+        fb: string,
+        insta: string,
+        discord: string,
+        youtube: string,
+      },
+}
