48cefcc4afcb7165f5c30a2c43d147a815503b94
rudrodip <official.rudrodipsarker@gmail.com>
2023-10-29 22:31:56 +0600

    types added

--- src/types/index.d.ts
+++ src/types/index.d.ts
@@ -12,3 +12,34 @@ export type SiteConfig = {
         youtube: string,
       },
 }
+
+type section<T> = {
+  caption: string,
+  categories: category<T>[]
+}
+
+type category<T> = {
+  name: string,
+  icon: string,
+  items: T[]
+}
+
+type image = {
+  mobile: string,
+  desktop: string
+}
+
+type defaultProfile = {
+  name: string,
+  url?: string,
+  image?: string,
+}
+
+type item = {
+  title: string,
+  desc: string,
+  image: image,
+  links: defaultProfile[],
+  otherLinks: defaultProfile[],
+  state?: boolean;
+}
