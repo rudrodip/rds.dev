15c0684e989be90d3cd954441e2b9c667f46f924
rudrodip <official.rudrodipsarker@gmail.com>
2023-10-30 11:45:04 +0600

    blank section init

---
+++ src/config/books.ts
@@ -0,0 +1,26 @@
+import { item, section } from "@src/types";
+
+const physicsBooks: item[] = []
+const mathBooks: item[] = []
+const csBooks: item[] = []
+
+export const books: section<item> = {
+  caption: 'Books',
+  categories: [
+    {
+      name: 'Physics',
+      icon: "/categories/featured.png",
+      items: physicsBooks,
+    },
+    {
+      name: 'Math',
+      icon: "/categories/featured.png",
+      items: mathBooks
+    },
+    {
+      name: 'Computer Science',
+      icon: "/categories/featured.png",
+      items: csBooks
+    }
+  ]
+}
