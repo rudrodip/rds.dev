f859a36db8027104d1380f74cb006ac3f958acf0
rudrodip <official.rudrodipsarker@gmail.com>
2023-10-30 11:44:09 +0600

    dynamic component load

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,30 @@
 import Header from "@src/components/Header/Header";
-import TerminalUI from "@src/components/TerminalUI/TerminalUI";
 import CodeQuote from "@src/components/Typographics/CodeQuote";
-import ContactPage from "@src/components/Contact/contact";
-import Projects from "@src/components/Project/Projects";
+import dynamic from "next/dynamic";
+import LoadSpinner from "@src/components/loadspinner";
+
+const DynamicTerminalUI = dynamic(() => import("@src/components/TerminalUI/TerminalUI"), { loading: () => <LoadSpinner /> });
+const DynamicProject = dynamic(() => import("@src/components/Project/Projects"), { loading: () => <LoadSpinner /> });
+const DynamicContactPage = dynamic(() => import("@src/components/Contact/contact"), { loading: () => <LoadSpinner /> });
 
 export default function Home() {
   return (
     <>
-      <div className="flex items-center min-h-screen -translate-y-10">
+      <div className="flex items-center md:min-h-screen my-6 md:mt-0 lg:-translate-y-10">
         <Header />
       </div>
-      <div id="aboutme" className="lg:mb-32">
+      <div id="aboutme" className=" mb-6 lg:mb-32">
         <CodeQuote />
       </div>
-      <TerminalUI />
-      <Projects />
-      <ContactPage />
+      <div className="lg:mb-32">
+        <DynamicTerminalUI />
+      </div>
+      <div id="projects" className="w-full lg:mb-32">
+        <DynamicProject />
+      </div>
+      <div className="lg:mb-32">
+        <DynamicContactPage />
+      </div>
     </>
   );
 }
