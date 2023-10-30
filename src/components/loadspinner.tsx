85bda3083b22a7b83632fdaf807c6fc4e565796c
rudrodip <official.rudrodipsarker@gmail.com>
2023-10-30 11:44:15 +0600

    updated spinner

--- src/components/loadspinner.tsx
+++ src/components/loadspinner.tsx
@@ -1,7 +1,9 @@
+import '@src/styles/loader.css'
+
 const LoadSpinner = () => {
   return (
     <div className="w-32 h-32 flex-center">
-      <div className="border-t-transparent border-solid animate-spin rounded-full border-primary border-8 h-16 w-16"></div>
+      <div className="loader"></div>
     </div>
   )
 }
