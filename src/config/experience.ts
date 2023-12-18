01f7e0ac13f50a05c60906c339dfedbfbfce6af4
rudrodip <official.rudrodipsarker@gmail.com>
2023-12-18 22:20:00 +0600

    Add experiences configuration file

---
+++ src/config/experience.ts
@@ -0,0 +1,48 @@
+import type { Experience } from "@src/types";
+
+export const experiencesConfig: Experience[] = [
+  {
+    title: "Robot Programmer",
+    employmentType: "Internship",
+    company: {
+      name: "Research Lab Bangladesh",
+      url: "https://www.facebook.com/ResearchLabCtg/",
+    },
+    location: { name: "Remote" },
+    start: "December, 2023",
+    end: "Present",
+    description: [],
+  },
+
+  {
+    title: "Secretary of IT",
+    employmentType: "Full-time",
+    company: {
+      name: "Rajshahi College Science Club",
+      url: "https://rcsc.vercel.app",
+    },
+    location: {
+      name: "Rajshahi College, Rajshahi, Bangladesh",
+      url: "https://rc.edu.bd",
+    },
+    start: "November, 2022",
+    end: "October, 2023",
+    description: [
+      "Managed the IT department of the club",
+      "Built the official website of the club",
+      "Supervised competitive programming under Rajshahi College National Science Fest 2023",
+    ],
+  },
+  {
+    title: "Web developer",
+    employmentType: "Open Source contributor",
+    company: {
+      name: "LunCo",
+      url: "https://lunco.space",
+    },
+    location: { name: "Remote" },
+    start: "December, 2023",
+    end: "December",
+    description: ["Developed the official website of LunCo"],
+  },
+];
