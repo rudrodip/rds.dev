67bfadd6d660099d057cf43c683142d83a4ed494
rudrodip <official.rudrodipsarker@gmail.com>
2023-12-18 22:21:49 +0600

    Refactor types and add new types for NavbarConfig, AboutConfig, Experience, and SocialIconProps

--- src/types/index.d.ts
+++ src/types/index.d.ts
@@ -1,45 +1,99 @@
+import type { Icon } from "lucide-react";
+import { LucideIcon } from "lucide-react";
+
 export type SiteConfig = {
-    name: string,
-    description: string,
-    url: string,
-    ogImage: string,
-    links: {
-        twitter: string,
-        github: string,
-        fb: string,
-        insta: string,
-        discord: string,
-        youtube: string,
-      },
-}
+  name: string;
+  description: string;
+  url: string;
+  ogImage: string;
+  links: {
+    twitter: string;
+    github: string;
+    fb: string;
+    insta: string;
+    discord: string;
+    youtube: string;
+  };
+};
 
 type section<T> = {
-  caption: string,
-  categories: category<T>[]
-}
+  caption: string;
+  categories: category<T>[];
+};
 
 type category<T> = {
-  name: string,
-  icon: string,
-  items: T[]
-}
+  name: string;
+  icon: string;
+  items: T[];
+};
 
 type image = {
-  mobile: string,
-  desktop: string
-}
+  mobile: string;
+  desktop: string;
+};
 
 type defaultProfile = {
-  name: string,
-  url?: string,
-  image?: string,
-}
+  name: string;
+  url?: string;
+  image?: string;
+};
 
 type item = {
-  title: string,
-  desc: string,
-  image: image,
-  links: defaultProfile[],
-  otherLinks: defaultProfile[],
+  title: string;
+  desc: string;
+  image: image;
+  links: defaultProfile[];
+  otherLinks: defaultProfile[];
   state?: boolean;
-}
+};
+
+export type NavItem = {
+  title: string;
+  href: string;
+  disabled?: boolean;
+  menu?: () => JSX.Element;
+};
+
+export type miniNavItem = {
+  href: string;
+  icon: Icon;
+};
+
+export type NavbarConfig = {
+  mainNav: NavItem[];
+  miniNav: miniNavItem[];
+};
+
+export type AboutConfig = {
+  name: string;
+  bio: string;
+  description: string;
+  image: string;
+  address: string;
+  phone: string;
+  links: {
+    twitter: string;
+    github: string;
+    fb: string;
+    insta: string;
+    discord: string;
+    youtube: string;
+  };
+};
+
+export type Experience = {
+  title: string;
+  employmentType: string;
+  company: defaultProfile;
+  location: defaultProfile;
+  start: string;
+  end: string;
+  description: string[];
+};
+
+export type SocialIconProps = {
+  Icon: LucideIcon;
+  accountName: string;
+  link: string;
+  pfpLink?: string;
+};
