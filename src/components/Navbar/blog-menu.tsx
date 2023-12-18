2aeed102620f8a84003e0c5dbc7f75ec5b4564d9
rudrodip <official.rudrodipsarker@gmail.com>
2023-12-18 23:09:02 +0600

    Update href in BlogMenu component

--- src/components/Navbar/blog-menu.tsx
+++ src/components/Navbar/blog-menu.tsx
@@ -13,7 +13,7 @@ export const BlogMenu = () => {
     <ul className="grid w-[400px] gap-3 p-4 md:w-[500px] md:grid-cols-2 lg:w-[600px] ">
       {posts.map((post, id) => {
         return (
-          <ListItem key={id} title={post.title} href="#projects">
+          <ListItem key={id} title={post.title} href={post.slug}>
             {post.description}
           </ListItem>
         );
