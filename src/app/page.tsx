913d8fd1660fa3788ce58cb157177f2f5f97c9ff
rudrodip <official.rudrodipsarker@gmail.com>
2023-12-18 22:18:45 +0600

    Add HeroSection and TechStack components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,21 +2,34 @@ import Header from "@src/components/Header/Header";
 import CodeQuote from "@src/components/Typographics/CodeQuote";
 import dynamic from "next/dynamic";
 import LoadSpinner from "@src/components/loadspinner";
+import { HeroSection } from "@src/components/Header/Hero";
+import { TechStack } from "@src/components/TechStack/TechStack";
 
-const DynamicTerminalUI = dynamic(() => import("@src/components/TerminalUI/TerminalUI"), { loading: () => <LoadSpinner /> });
-const DynamicProject = dynamic(() => import("@src/components/Project/Projects"), { loading: () => <LoadSpinner /> });
-const DynamicMobileProject = dynamic(() => import("@src/components/Project/MobileView"), { loading: () => <LoadSpinner /> });
-const DynamicContactPage = dynamic(() => import("@src/components/Contact/contact"), { loading: () => <LoadSpinner /> });
+const DynamicTerminalUI = dynamic(
+  () => import("@src/components/TerminalUI/TerminalUI"),
+  { loading: () => <LoadSpinner /> }
+);
+const DynamicProject = dynamic(
+  () => import("@src/components/Project/Projects"),
+  { loading: () => <LoadSpinner /> }
+);
+const DynamicMobileProject = dynamic(
+  () => import("@src/components/Project/MobileView"),
+  { loading: () => <LoadSpinner /> }
+);
+const DynamicContactPage = dynamic(
+  () => import("@src/components/Contact/contact"),
+  { loading: () => <LoadSpinner /> }
+);
 
 export default function Home() {
   return (
     <>
-      <div className="flex items-center md:min-h-screen my-6 md:mt-0 lg:-translate-y-10">
-        <Header />
-      </div>
-      <div id="aboutme" className=" mb-6 lg:mb-32">
+      <HeroSection />
+      <div id="aboutme" className="mt-10 mb-6 lg:mb-32">
         <CodeQuote />
       </div>
+      {/* <TechStack /> */}
       <div className="lg:mb-32">
         <DynamicTerminalUI />
       </div>
