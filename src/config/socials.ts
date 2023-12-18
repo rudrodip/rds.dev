b30a719286bb5e109f8e9331af24f16e3b0e8752
rudrodip <official.rudrodipsarker@gmail.com>
2023-12-18 22:21:36 +0600

    Add social icons configuration

---
+++ src/config/socials.ts
@@ -0,0 +1,35 @@
+import { SocialIconProps } from "@src/types";
+import { Github, Twitter, Linkedin, Facebook, Instagram } from "lucide-react";
+
+export const socialIconsData: SocialIconProps[] = [
+  {
+    Icon: Github,
+    accountName: "rudrodip",
+    link: "https://github.com/rudrodip",
+    pfpLink: "https://avatars.githubusercontent.com/u/77154365",
+  },
+  {
+    Icon: Twitter,
+    accountName: "@rds_agi",
+    link: "https://twitter.com/rds_agi",
+    pfpLink: "https://unavatar.io/twitter/rds_agi",
+  },
+  {
+    Icon: Linkedin,
+    accountName: "Rudrodip Sarker",
+    link: "https://www.linkedin.com/in/rudrodip",
+    pfpLink: "https://avatars.githubusercontent.com/u/77154365",
+  },
+  {
+    Icon: Facebook,
+    accountName: "Rudrodip Sarker",
+    link: "https://facebook.com/enthusiast.math",
+    pfpLink: "https://avatars.githubusercontent.com/u/77154365",
+  },
+  {
+    Icon: Instagram,
+    accountName: "@rds_agi",
+    link: "https://www.instagram.com/rds_agi",
+    pfpLink: "https://avatars.githubusercontent.com/u/77154365",
+  },
+];
