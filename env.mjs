57f3b236b35eee2d62acb85c4512567be51bceda
rudrodip <official.rudrodipsarker@gmail.com>
2023-12-18 22:17:20 +0600

    Add NEXT_PUBLIC_ALCHEMY_API_KEY to env.mjs

--- env.mjs
+++ env.mjs
@@ -1,11 +1,13 @@
-import { createEnv } from "@t3-oss/env-nextjs"
-import { z } from "zod"
+import { createEnv } from "@t3-oss/env-nextjs";
+import { z } from "zod";
 
 export const env = createEnv({
   client: {
     NEXT_PUBLIC_APP_URL: z.string().min(1),
+    NEXT_PUBLIC_ALCHEMY_API_KEY: z.string().min(1),
   },
   runtimeEnv: {
     NEXT_PUBLIC_APP_URL: process.env.NEXT_PUBLIC_APP_URL,
+    NEXT_PUBLIC_ALCHEMY_API_KEY: process.env.NEXT_PUBLIC_ALCHEMY_API_KEY,
   },
-})
+});
