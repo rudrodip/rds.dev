10f3ca9d66fb32c4fa6988bdb30328840bf98442
rudrodip <official.rudrodipsarker@gmail.com>
2024-02-08 19:31:04 +0600

    Add new experiences to experiencesConfig

--- src/config/experience.ts
+++ src/config/experience.ts
@@ -2,6 +2,52 @@ import type { Experience } from "@src/types";
 
 export const experiencesConfig: Experience[] = [
   {
+    title: "Head of IT",
+    employmentType: "Core Team member",
+    company: {
+      name: "Bettre Institute",
+      url: "https://bettreinstitute.com",
+    },
+    location: { name: "Remote" },
+    start: "November, 2023",
+    end: "Present",
+    description: [
+      "Currently developing AI solutions for empowering L.M.S in our platform",
+      "Developed the officiail website for Bettre Institute",
+    ],
+  },
+  {
+    title: "Fullstack developer",
+    employmentType: "Core Team member",
+    company: {
+      name: "Pandasticals",
+      url: "https://twitter.com/Pandasticals",
+    },
+    location: { name: "Remote" },
+    start: "January, 2023",
+    end: "Februrary, 2023",
+    description: [
+      "Currently developing distributed stateless NFT art engine",
+      "Developed the core product of Pandasticals",
+      "Built automation tools",
+    ],
+  },
+  {
+    title: "Fullstack developer",
+    employmentType: "Open Source contributor",
+    company: {
+      name: "LunCo",
+      url: "https://lunco.space",
+    },
+    location: { name: "Remote" },
+    start: "December, 2023",
+    end: "Present",
+    description: [
+      "Currently adding more features to the web faced product of LunCo",
+      "Developed the official website of LunCo",
+    ],
+  },
+  {
     title: "Robot Programmer",
     employmentType: "Internship",
     company: {
@@ -13,7 +59,6 @@ export const experiencesConfig: Experience[] = [
     end: "Present",
     description: [],
   },
-
   {
     title: "Secretary of IT",
     employmentType: "Full-time",
@@ -32,17 +77,5 @@ export const experiencesConfig: Experience[] = [
       "Built the official website of the club",
       "Supervised competitive programming under Rajshahi College National Science Fest 2023",
     ],
-  },
-  {
-    title: "Web developer",
-    employmentType: "Open Source contributor",
-    company: {
-      name: "LunCo",
-      url: "https://lunco.space",
-    },
-    location: { name: "Remote" },
-    start: "December, 2023",
-    end: "December",
-    description: ["Developed the official website of LunCo"],
   },
 ];
