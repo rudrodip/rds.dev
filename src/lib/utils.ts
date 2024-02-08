e45b8151d1d49fdd08d14c615202c0fad71db0d1
rudrodip <official.rudrodipsarker@gmail.com>
2024-02-08 19:31:10 +0600

    Add hexToRGBA function to utils.ts

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -32,3 +32,15 @@ export const formatChainAsNum = (chainIdHex: string) => {
 export const formatAddress = (addr: string | undefined) => {
   return `${addr?.substring(0, 8)}...`;
 };
+
+export function hexToRGBA(hex: string, alpha: number): string {
+  let r = parseInt(hex.slice(1, 3), 16),
+    g = parseInt(hex.slice(3, 5), 16),
+    b = parseInt(hex.slice(5, 7), 16);
+
+  if (alpha) {
+    return `rgba(${r}, ${g}, ${b}, ${alpha})`;
+  } else {
+    return `rgb(${r}, ${g}, ${b})`;
+  }
+}
