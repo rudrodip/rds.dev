73a1e1622fcd003bbbde611830a3a7b066b5a89a
rudrodip <official.rudrodipsarker@gmail.com>
2024-02-08 19:30:42 +0600

    Add ParallaxText component for animated text

---
+++ src/components/Header/Parallax.tsx
@@ -0,0 +1,64 @@
+import { useRef } from "react";
+import {
+  motion,
+  useScroll,
+  useSpring,
+  useTransform,
+  useMotionValue,
+  useVelocity,
+  useAnimationFrame,
+} from "framer-motion";
+import { wrap } from "@motionone/utils";
+
+interface ParallaxProps {
+  children: JSX.Element | JSX.Element[] | string | string[];
+  baseVelocity: number;
+}
+
+export function ParallaxText({ children, baseVelocity = 100 }: ParallaxProps) {
+  const baseY = useMotionValue(0);
+  const { scrollY } = useScroll();
+  const scrollVelocity = useVelocity(scrollY);
+  const smoothVelocity = useSpring(scrollVelocity, {
+    damping: 50,
+    stiffness: 400,
+  });
+  const velocityFactor = useTransform(smoothVelocity, [0, 1000], [0, 5], {
+    clamp: false,
+  });
+  const y = useTransform(baseY, (v) => `${wrap(-20, 20, v)}%`);
+
+  const directionFactor = useRef<number>(1);
+  useAnimationFrame((t, delta) => {
+    let moveBy = directionFactor.current * baseVelocity * (delta / 1000);
+    if (velocityFactor.get() < 0) {
+      directionFactor.current = -1;
+    } else if (velocityFactor.get() > 0) {
+      directionFactor.current = 1;
+    }
+
+    moveBy += directionFactor.current * moveBy * velocityFactor.get();
+
+    baseY.set(baseY.get() + moveBy);
+  });
+
+  return (
+    <div className="parallax">
+      <motion.div
+        className="flex flex-col justify-center items-center gap-2"
+        style={{ y }}
+      >
+        <span>{children}</span>
+        <span>{children}</span>
+        <span>{children}</span>
+        <span>{children}</span>
+        <span>{children}</span>
+        <span>{children}</span>
+        <span>{children}</span>
+        <span>{children}</span>
+        <span>{children}</span>
+        <span>{children}</span>
+      </motion.div>
+    </div>
+  );
+}
