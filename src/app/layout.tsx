40c4a8872f6e6293175e367fe24b31783ef238c2
rudrodip <official.rudrodipsarker@gmail.com>
2024-02-08 19:29:47 +0600

    Update header styling in layout component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -117,7 +117,7 @@ export default function RootLayout({
           <Provider>
             <div className="flex flex-col min-h-screen">
               <div className="flex-grow">
-                <header className="z-40 mx-2">
+                <header className="absolute top-0 left-0 right-0 z-40 mx-2">
                   <Navbar />
                 </header>
                 <MiniNavbar />
