00f9cadd148e97c64fd9120ae42d745686969140
rudrodip <official.rudrodipsarker@gmail.com>
2024-02-09 11:17:47 +0600

    Add RootCanvas component to handle canvas rendering

---
+++ src/components/Canvas/root-canvas.tsx
@@ -0,0 +1,33 @@
+"use client";
+
+import useMediaQuery from "@/hooks/use-media-query";
+import { useRef, useEffect, useState } from "react";
+
+export const RootCanvas = () => {
+  const canvasRef = useRef<HTMLCanvasElement>(null);
+  const { height, width } = useMediaQuery();
+
+  useEffect(() => {
+    const canvas = canvasRef.current;
+    if (!canvas) return;
+
+    const ctx = canvas?.getContext("2d");
+    if (!ctx) return;
+  }, []);
+
+  return (
+    <div>
+      <canvas
+        ref={canvasRef}
+        width={width}
+        height={height}
+        style={{
+          position: "fixed",
+          top: 0,
+          left: 0,
+          zIndex: -1,
+        }}
+      />
+    </div>
+  );
+};
