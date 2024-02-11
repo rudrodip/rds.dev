76c32ef7c6c0ad36e9d02880dd65668182a35ea4
rudrodip <official.rudrodipsarker@gmail.com>
2024-02-11 09:55:06 +0600

    Update media queries in GenAI3 component

--- src/components/Projects/genai/genai3.tsx
+++ src/components/Projects/genai/genai3.tsx
@@ -15,7 +15,9 @@ const motionConfig = {
 };
 
 export default function GenAI3() {
-  const isMobile = useMediaQuery("(max-width: 640px)");
+  const isMobile = useMediaQuery("(max-width: 1000px)");
+  const isTablet = useMediaQuery("(max-width: 1610px)");
+
   const techStack = [
     "Next.js",
     "Supabase",
@@ -58,7 +60,8 @@ export default function GenAI3() {
         <motion.div
           className={cn(
             "absolute flex flex-col gap-3",
-            isMobile ? "hidden" : "top-40 -left-40"
+            isTablet ? "top-40 -left-2" : "top-40 -left-40",
+            isMobile && "hidden"
           )}
           {...motionConfig}
           transition={{ duration: 0.25 }}
