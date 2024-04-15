e536194929fb887123a2f437221ad4c57670bac0
rudrodip <official.rudrodipsarker@gmail.com>
2024-04-15 15:51:32 +0600

    Update HeroSection in src/components/Header/Hero.tsx

--- src/components/Header/Hero.tsx
+++ src/components/Header/Hero.tsx
@@ -5,34 +5,34 @@ import { ExperienceSection } from "./Experience";
 import SocialIcons from "@/components/Socials/SocialIcons";
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
-import DiscordApperance from "../Socials/DiscordApperance";
 import Image from "next/image";
 
 export const HeroSection = () => {
   return (
-    <section className="w-full flex justify-center items-center my-12 lg:my-24">
+    <section className="w-full flex justify-center items-center my-12">
       <div className="w-full md:w-1/2 xl:w-full flex flex-col justify-center">
         <div className="flex flex-col justify-center">
-          <h1 className="head-text primary-gradient">{aboutConfig.name}</h1>
-          <span className="text-xs primary-gradient">
-            aka{" "}
+          <h1 className="head-text">{aboutConfig.name}</h1>
+          <span className="text-sm">
+            a.k.a.{" "}
             <a
               href={aboutConfig.links.twitter}
               target="_blank"
-              className="text-rose-400 underline"
+              className="underline"
             >
               @rds_agi
             </a>
           </span>
         </div>
-        <p className="desc primary-gradient">
+        <p className="desc">
           {aboutConfig.bio.substring(0, aboutConfig.bio.lastIndexOf(" "))}{" "}
-          <span className="before:block before:absolute before:-inset-1 before:skew-y-3 before:bg-secondary relative inline-block">
+          <span className="before:block before:absolute before:-inset-1 before:skew-y-3 before:bg-accent relative inline-block">
             <span className="relative">
               {aboutConfig.bio.slice(aboutConfig.bio.lastIndexOf(" ") + 1)}
             </span>
           </span>
         </p>
+        <p className="text-sm tracking-wide">stack agnostic developer &gt; Jack of all trades, master of some</p>
         <div className="flex gap-4 align-middle flex-wrap my-5">
           <Button asChild variant="outline" size="sm">
             <Link href="/#contact">Get in touch</Link>
