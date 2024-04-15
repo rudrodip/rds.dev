6623b01b4468659968929ce937378a6aaa89a2bd
rudrodip <official.rudrodipsarker@gmail.com>
2024-04-15 15:51:37 +0600

    Update MiniNavbar component in src/components/Navbar/MiniNavbar.tsx

--- src/components/Navbar/MiniNavbar.tsx
+++ src/components/Navbar/MiniNavbar.tsx
@@ -12,7 +12,7 @@ import {
   TooltipProvider,
   TooltipTrigger,
 } from "@/components/ui/tooltip";
-import { SearchBar } from "./search-bar";
+// import { SearchBar } from "./search-bar";
 import useMediaQuery from "@src/hooks/use-media-query";
 
 export const MiniNavbar = () => {
@@ -24,37 +24,39 @@ export const MiniNavbar = () => {
       className={cn(
         "flex justify-center sticky top-2 z-[1000] transition-all transform-gpu duration-200",
         scrolledEnough
-          ? "opacity-100 transalte-y-0 scale-100"
-          : "opacity-0 -translate-y-24 scale-0"
+          ? "opacity-100 transalte-y-0"
+          : "opacity-0 -translate-y-24"
       )}
     >
-      <div className="border border-primary/10 bg-secondary/50 backdrop-blur-lg">
-        <div className="flex justify-between gap-5 items-center px-1 py-1">
-          {navbarConfig.miniNav.map((item, index) => (
-            <TooltipProvider key={index} delayDuration={200}>
-              <Tooltip>
-                <TooltipTrigger asChild>
-                  <Link
-                    href={item.href}
-                    className="transition-all transform-gpu hover:scale-110 duration-75"
-                  >
-                    <Button
-                      variant="outline"
-                      size="icon"
-                      className="bg-transparent border-none"
+      <div className="bg-transparent navbar-clip">
+        <div className="navbar-clip-border"></div>
+        <div className="navbar-clip-content">
+          <div className="navbar-clip-border"></div>
+          <div className="flex justify-between gap-5 items-center px-5 py-1">
+            {navbarConfig.miniNav.map((item, index) => (
+              <TooltipProvider key={index} delayDuration={200}>
+                <Tooltip>
+                  <TooltipTrigger asChild>
+                    <Link
+                      href={item.href}
                     >
-                      <item.icon className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all" />
-                    </Button>
-                  </Link>
-                </TooltipTrigger>
-                <TooltipContent>
-                  <p>{item.href}</p>
-                </TooltipContent>
-              </Tooltip>
-            </TooltipProvider>
-          ))}
-          {isMobile ? null : <SearchBar />}
-          <ThemeToggleDropDown />
+                      <Button
+                        variant="outline"
+                        size="icon"
+                        className="bg-transparent rounded-full border-none"
+                      >
+                        <item.icon className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all" />
+                      </Button>
+                    </Link>
+                  </TooltipTrigger>
+                  <TooltipContent>
+                    <p>{item.href}</p>
+                  </TooltipContent>
+                </Tooltip>
+              </TooltipProvider>
+            ))}
+            <ThemeToggleDropDown />
+          </div>
         </div>
       </div>
     </section>
