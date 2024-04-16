3ab04524fee6e868fb921c041d1987ffa0ba6e1a
rudrodip <official.rudrodipsarker@gmail.com>
2024-04-16 23:46:53 +0600

    Add ProjectSm component to display small project details on Github page

---
+++ src/components/Github/project-sm.tsx
@@ -0,0 +1,36 @@
+import type { Repository } from "@src/types/github";
+import { Star } from "lucide-react";
+
+export default function ProjectSm({
+  url,
+  name,
+  description,
+  primaryLanguage,
+  stargazerCount,
+  forkCount,
+}: Repository) {
+  return (
+    <div className="group w-full max-w-lg mx-auto p-2 bg-secondary border hover:bg-accent transform-gpu transition-colors duration-200">
+      <a className="text-lg font-bold underline" href={url} target="_blank">
+        {name}
+      </a>
+      <p className="text-sm truncate">{description}</p>
+      <div className="flex gap-2 mt-2 items-center">
+        <p className="text-sm bg-accent/50 group-hover:bg-secondary/50 p-1">
+          {primaryLanguage.name}
+        </p>
+        {forkCount > 0 && (
+          <p className="text-sm bg-accent/50 group-hover:bg-secondary/50 p-1">
+            Forks: {forkCount}
+          </p>
+        )}
+        {stargazerCount > 0 && (
+          <div className="group/star text-sm p-1 flex items-center gap-1 cursor-pointer">
+            <Star size={14} />
+            {stargazerCount}
+          </div>
+        )}
+      </div>
+    </div>
+  );
+}
