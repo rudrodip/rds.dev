218540ee778a27b418fde8c41c8c9e3dd1d8145d
rudrodip <official.rudrodipsarker@gmail.com>
2024-04-16 23:46:32 +0600

    Update loading component styles in src/app/loading.tsx

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -2,7 +2,7 @@ import LoadSpinner from "@/components/loadspinner";
 
 const loading = () => {
   return (
-    <div className="w-full min-h-screen flex-center">
+    <div className="w-full min-h-screen flex justify-center items-center">
       <LoadSpinner />
     </div>
   );
