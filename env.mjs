f4b55606bee0ae09ba4c3667f1a5764866a089f0
rudrodip <official.rudrodipsarker@gmail.com>
2024-04-16 23:46:09 +0600

    Update environment variables in env.mjs

--- env.mjs
+++ env.mjs
@@ -6,8 +6,12 @@ export const env = createEnv({
     NEXT_PUBLIC_APP_URL: z.string().min(1),
     NEXT_PUBLIC_ALCHEMY_API_KEY: z.string().min(1),
   },
+  server: {
+    GITHUB_AUTH_TOKEN: z.string().min(1),
+  },
   runtimeEnv: {
     NEXT_PUBLIC_APP_URL: process.env.NEXT_PUBLIC_APP_URL,
     NEXT_PUBLIC_ALCHEMY_API_KEY: process.env.NEXT_PUBLIC_ALCHEMY_API_KEY,
+    GITHUB_AUTH_TOKEN: process.env.GITHUB_AUTH_TOKEN,
   },
 });
