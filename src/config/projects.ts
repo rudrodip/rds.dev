0dc2b8d58469028d13e4d3143a9ba07013eb462b
rudrodip <official.rudrodipsarker@gmail.com>
2024-04-17 15:14:15 +0600

    Refactor code

--- src/config/projects.ts
+++ src/config/projects.ts
@@ -1,643 +1,92 @@
-import { item, section } from "@/types";
+import type { Project } from "@src/types";
 
-const webProjects: item[] = [
+export const projects: Project[] = [
   {
-    title: "NexusOS",
-    desc: "NexusOS is a platform that empowers researchers, scientists, and innovators to come together and collaborate on impactful scientific projects. Discover, join, or create open science initiatives across diverse fields. Join us in shaping the future of science and discovery.",
-    otherLinks: [
-      {
-        name: "Next.js 13",
-        url: "https://nextjs.org/",
-        image: "/assets/images/techlogo/nextjs-transparent.svg",
-      },
-      {
-        name: "TailwindCSS",
-        image: "/assets/images/techlogo/tailwind.svg",
-      },
-      {
-        name: "Typescript",
-        image: "/assets/images/techlogo/ts.svg",
-      },
-      {
-        name: "Redux",
-        image: "/assets/images/techlogo/redux.svg",
-      },
-      {
-        name: "ShadCn",
-      },
-      {
-        name: "Zenodo",
-      },
-      {
-        name: "GPT-3.5-turbo",
-      },
+    name: "Sonicrypt",
+    slogan: "Bring sound and sight to your silent transactions",
+    description: "Sonicrypt is a compact and intuitive tool designed to enhance the user experience for both buyers and sellers engaging in crypto transactions. Its primary function is to provide immediate auditory and visual confirmation of successful payments, thereby instilling confidence and trust in digital transactions",
+    relatedRepos: [
+      "https://github.com/Sonicrypt",
+      "https://github.com/Sonicrypt/sonicrypt",
+      "https://github.com/Sonicrypt/sonicrypt-app",
+      "https://github.com/Sonicrypt/sonicrypt-webapp",
     ],
-    links: [
-      {
-        name: "NexusOS",
-        url: "https://nexusos.vercel.app",
-      },
-      {
-        name: "Github",
-        url: "https://github.com/rudrodip/nexusos",
-      },
-    ],
-    image: {
-      mobile: "/projects/nexusos-mobile.jpg",
-      desktop: "/projects/nexusos-desktop.jpg",
+    media: {
+      video: "https://firebasestorage.googleapis.com/v0/b/collabsync-yt.appspot.com/o/white-theme.mp4?alt=media&token=34acc426-7e1e-4588-be73-aca560871353",
+      images: [
+        "https://sonicrypt.vercel.app/og.png"
+      ],
     },
+    achievements: [],
+    tech: [
+      "ESP32-S3", "C++", "Solana web3js", "React Native", "Nextjs 14"
+    ],
+    links: {
+      "Website": "https://sonicrypt.vercel.app",
+      "Demo": "https://www.youtube.com/watch?v=yUm7kYV3p28",
+      "App": "https://github.com/Sonicrypt/sonicrypt-app/releases",
+      "Twitter/X": "https://twitter.com/sonicrypt",
+    }
   },
   {
-    title: "rds.dev",
-    desc: "Portfolio website for Rudrodip Sarker",
-    links: [
-      {
-        name: "Website",
-        url: "https://rudrodip.tech",
-      },
-      {
-        name: "Github",
-        url: "https://github.com/rudrodip/rds.dev",
-      },
+    name: "NexusOS",
+    slogan: "Connecting Minds, Advancing Science",
+    description: "NexusOS is a platform that empowers researchers, scientists, and innovators to come together and collaborate on impactful scientific projects. Discover, join, or create open science initiatives across diverse fields. Join us in shaping the future of science and discovery",
+    relatedRepos: [
+      "https://github.com/rudrodip/NexusOS",
     ],
-    otherLinks: [
-      {
-        name: "Next.js 13",
-        url: "https://nextjs.org/",
-        image: "/assets/images/techlogo/nextjs-transparent.svg",
-      },
-      {
-        name: "TailwindCSS",
-        image: "/assets/images/techlogo/tailwind.svg",
-      },
-      {
-        name: "Typescript",
-        image: "/assets/images/techlogo/ts.svg",
-      },
-      {
-        name: "ShadCn",
-      },
-    ],
-    image: {
-      mobile: "/projects/rds.dev-mobile.png",
-      desktop: "/projects/rds.dev-desktop.png",
+    media: {
+      video: "https://firebasestorage.googleapis.com/v0/b/hmmmhmmmhh.appspot.com/o/n.mp4?alt=media&token=bbd5ffed-cb00-4a54-98a4-f989887644bd",
+      images: [
+        "/projects/featured1/thumbnail.png"
+      ],
     },
+    achievements: [
+      "Selected as a Global Nominee for NASA Space Apps Challenge 2023"
+    ],
+    tech: [
+      "Next.js",
+      "Next-Auth",
+      "Supabase",
+      "Prisma",
+      "Shadcn UI",
+      "Github octokit",
+      "TailwindCSS",
+      "Framer Motion",
+    ],
+    links: {
+      "Website": "https://nexusos.vercel.app",
+      "Demo": "https://youtu.be/xZwEQJX_JbQ",
+    }
   },
   {
-    title: "RCSC",
-    desc: "Official website for Rajshahi College Science Club",
-    links: [
-      {
-        name: "RCSC",
-        url: "https://rcsc.vercel.app",
-      },
-      {
-        name: "Github",
-        url: "https://github.com/rudrodip/rcsc",
-      },
+    name: "Harmonic Motion Analyzer",
+    slogan: "Leveraging Computer Vision for harmonic motion analysis",
+    description: "This Python project is designed to analyze the harmonic oscillation of an object using computer vision techniques. It leverages various libraries, including OpenCV for video feed processing, SciPy for data analysis, NumPy for numerical operations, and PyQt5 for creating a graphical user interface (GUI). The project detects and tracks an object in a video feed, extracts its motion data, fits it to a damped oscillation function, and calculates physical parameters such as pendulum length and pivot point location",
+    relatedRepos: [
+      "https://github.com/rudrodip/Harmonic-Oscillator-CV"
     ],
-    otherLinks: [
-      {
-        name: "Next.js 12",
-        url: "https://nextjs.org/",
-        image: "/assets/images/techlogo/nextjs-transparent.svg",
-      },
-      {
-        name: "TailwindCSS",
-        image: "/assets/images/techlogo/tailwind.svg",
-      },
-      {
-        name: "Javascript",
-        image: "/assets/images/techlogo/js.svg",
-      },
-      {
-        name: "Firebase",
-      },
-    ],
-    image: {
-      mobile: "/projects/rcsc-mobile.png",
-      desktop: "/projects/rcsc-desktop.png",
+    media: {
+      video: "https://firebasestorage.googleapis.com/v0/b/hmmmhmmmhh.appspot.com/o/h.mp4?alt=media&token=7d401557-583d-4872-9b95-a79230288d4d",
+      images: [
+        "/projects/featured2/thumbnail.png"
+      ],
     },
+    achievements: [
+      "Scored Highest in Creative Coding Competition organized by Radu Mariescu-Istodor"
+    ],
+    tech: [
+      "Python",
+      "OpenCV-headless",
+      "PyQt5",
+      "PyQtGraph",
+      "Matplotlib",
+      "Numpy",
+      "SciPy",
+    ],
+    links: {
+      "Blog": "https://rudrodip.tech/blog/harmonic-oscillation-analyzer",
+      "Demo": "https://www.youtube.com/watch?v=dalsCsHtreU&t=1220s",
+    }
   },
-  {
-    title: "CollabSync",
-    desc: "CollabSync is the go-to platform for connecting creators and editors, streamlining content creation and fostering collaboration in the digital world",
-    links: [
-      {
-        name: "Github",
-        url: "https://github.com/rudrodip/collabsync",
-      },
-    ],
-    otherLinks: [
-      {
-        name: "Next.js 13",
-        url: "https://nextjs.org/",
-        image: "/assets/images/techlogo/nextjs-transparent.svg",
-      },
-      {
-        name: "TailwindCSS",
-        image: "/assets/images/techlogo/tailwind.svg",
-      },
-      {
-        name: "Typescript",
-        image: "/assets/images/techlogo/ts.svg",
-      },
-      {
-        name: "Redux",
-        image: "/assets/images/techlogo/redux.svg",
-      },
-      {
-        name: "ShadCn",
-      },
-    ],
-    image: {
-      mobile: "/projects/collabsync-mobile.png",
-      desktop: "/projects/collabsync-desktop.png",
-    },
-  },
-  {
-    title: "TopsNet",
-    desc: "TOPSNET is a professional web application built to accelerate NASA's transformation to open science goals. The platform enables researchers to discover, share, and collaborate on open research papers and resources.",
-    links: [
-      {
-        name: "Website",
-        url: "https://topsnet.vercel.app",
-      },
-      {
-        name: "Github",
-        url: "https://github.com/rudrodip/topsnet",
-      },
-    ],
-    otherLinks: [
-      {
-        name: "Next.js 13",
-        url: "https://nextjs.org/",
-        image: "/assets/images/techlogo/nextjs-transparent.svg",
-      },
-      {
-        name: "TailwindCSS",
-        image: "/assets/images/techlogo/tailwind.svg",
-      },
-      {
-        name: "Typescript",
-        image: "/assets/images/techlogo/ts.svg",
-      },
-      {
-        name: "Redux",
-        image: "/assets/images/techlogo/redux.svg",
-      },
-      {
-        name: "ShadCn",
-      },
-    ],
-    image: {
-      mobile: "/projects/topsnet-mobile.png",
-      desktop: "/projects/topsnet-desktop.png",
-    },
-  },
-];
-
-const simulationProjects: item[] = [
-  {
-    title: "Harmonic Oscillation Analyzer",
-    desc: "This Python project is designed to analyze the harmonic oscillation of an object using computer vision techniques.",
-    links: [
-      {
-        name: "Blog",
-        url: "https://rudrodip.vercel.app/blog/harmonic-oscillation-analyzer",
-      },
-      {
-        name: "Github",
-        url: "https://github.com/rudrodip/Harmonic-Oscillator-CV",
-      },
-    ],
-    otherLinks: [
-      {
-        name: "Python",
-        image: "/assets/images/techlogo/python.svg",
-      },
-      {
-        name: "OpenCV-headless",
-        image: "/assets/images/techlogo/opencv.svg",
-      },
-      {
-        name: "CVZone",
-      },
-      {
-        name: "PyQT5",
-      },
-      {
-        name: "PyQtGraph",
-      },
-      {
-        name: "Numpy",
-      },
-      {
-        name: "SciPy",
-      },
-      {
-        name: "Matplotlib",
-      },
-    ],
-    image: {
-      mobile: "/projects/harmonic-mobile.png",
-      desktop: "/projects/harmonic-desktop.png",
-    },
-  },
-  {
-    title: "Self Driving Car 2D Simulation",
-    desc: "Description for self-driving-car-simulation",
-    links: [
-      {
-        name: "Website",
-        url: "https://sdc-simulation.vercel.app",
-      },
-      {
-        name: "Github",
-        url: "https://github.com/rudrodip/self-driving-car-simulation",
-      },
-    ],
-    otherLinks: [
-      {
-        name: "Next.js 13",
-        url: "https://nextjs.org/",
-        image: "/assets/images/techlogo/nextjs-transparent.svg",
-      },
-      {
-        name: "Javascript",
-        image: "/assets/images/techlogo/js.svg",
-      },
-      {
-        name: "HTML Canvas",
-      },
-    ],
-    image: {
-      mobile: "/projects/sdc-mobile.png",
-      desktop: "/projects/sdc-desktop.png",
-    },
-  },
-];
-
-const backendProjects: item[] = [
-  {
-    title: "CollabSync api",
-    desc: "CollabSync is a collaborative video content management platform that allows YouTubers and content creators to manage their video production workflow. This backend repository contains the server-side code for CollabSync.",
-    links: [
-      {
-        name: "Github",
-        url: "https://github.com/rudrodip/collabsync-api",
-      },
-    ],
-    otherLinks: [
-      {
-        name: "Node JS",
-        image: "/assets/images/techlogo/nodejs.svg",
-      },
-      {
-        name: "Express",
-      },
-      {
-        name: "GCP",
-      },
-      {
-        name: "Youtube Data API v3",
-      },
-    ],
-    image: {
-      mobile: "/projects/collabsyncapi-desktop.png",
-      desktop: "/projects/collabsyncapi-desktop.png",
-    },
-  },
-  {
-    title: "Topsnet backend",
-    desc: "TOPSnet Backend is a Python-based server-side component for the TOPSNET web application. It fetches data from Firebase and other APIs, processes the data, and generates personalized research recommendations for users.",
-    links: [
-      {
-        name: "Github",
-        url: "https://github.com/rudrodip/topsnet-backend",
-      },
-    ],
-    otherLinks: [
-      {
-        name: "Python",
-      },
-      {
-        name: "Firebase",
-      },
-      {
-        name: "NLTK",
-      },
-      {
-        name: "HTML2TEXT",
-      },
-    ],
-    image: {
-      mobile: "/projects/topsnetbackend-desktop.png",
-      desktop: "/projects/topsnetbackend-desktop.png",
-    },
-  },
-  {
-    title: "Student Management API",
-    desc: "Express API for student management system",
-    links: [
-      {
-        name: "Github",
-        url: "https://github.com/rudrodip/student-management-api",
-      },
-    ],
-    otherLinks: [
-      {
-        name: "Node JS",
-      },
-      {
-        name: "Express",
-      },
-    ],
-    image: {
-      mobile: "/projects/smsapi-desktop.png",
-      desktop: "/projects/smsapi-desktop.png",
-    },
-  },
-];
-
-const botProjects: item[] = [
-  {
-    title: "IntelliLearn",
-    desc: "Intelli-Learn is an AI-powered educational Discord bot that aims to enhance the learning experience for users. It provides personalized learning assistance, interactive quizzes, and access to educational resources, all within the Discord platform.",
-    links: [
-      {
-        name: "Github",
-        url: "https://github.com/rudrodip/IntelliLearn",
-      },
-    ],
-    otherLinks: [
-      {
-        name: "Python",
-        image: "/assets/images/techlogo/python.svg",
-      },
-      {
-        name: "Pycord",
-      },
-      {
-        name: "OpenAI GPT-3",
-      },
-      {
-        name: "Langchain",
-      },
-    ],
-    image: {
-      mobile: "/projects/intellilearn-mobile.png",
-      desktop: "/projects/intellilearn-desktop.png",
-    },
-  },
-  {
-    title: "Eccentric",
-    desc: "Telegram bot that sends memes, jokes, quotes and cat pics 😉",
-    links: [
-      {
-        name: "Github",
-        url: "https://github.com/rudrodip/Eccentric",
-      },
-    ],
-    otherLinks: [
-      {
-        name: "Python",
-        image: "/assets/images/techlogo/python.svg",
-      },
-      {
-        name: "Python-telegram-bot",
-      },
-    ],
-    image: {
-      mobile: "/projects/eccentric-mobile.png",
-      desktop: "/projects/eccentric-desktop.png",
-    },
-  },
-  {
-    title: "Personal-assistant",
-    desc: "A discord bot serves as a personal assistant for users",
-    links: [
-      {
-        name: "Github",
-        url: "https://github.com/rudrodip/Personal-Assistant",
-      },
-    ],
-    otherLinks: [
-      {
-        name: "Python",
-        image: "/assets/images/techlogo/python.svg",
-      },
-      {
-        name: "Pycord",
-      },
-      {
-        name: "OpenAI GPT-3",
-      },
-    ],
-    image: {
-      mobile: "/projects/sergio-mobile.png",
-      desktop: "/projects/sergio-desktop.png",
-    },
-  },
-];
-
-const roboticsProjects: item[] = [
-  {
-    title: "AquaRover",
-    desc: "AquaRover is an innovative water monitoring solution aimed at providing accurate and real-time data on water quality parameters. We believe that access to information is essential in addressing water pollution and improving the quality of water resources",
-    links: [
-      {
-        name: "Github",
-        url: "https://github.com/rudrodip/AquaRover",
-      },
-    ],
-    otherLinks: [
-      {
-        name: "C++",
-        image: "/assets/images/techlogo/cpp.svg",
-      },
-      {
-        name: "Arduino",
-        image: "/assets/images/techlogo/arduino.svg",
-      },
-      {
-        name: "PlatformIO",
-      },
-      {
-        name: "ArduinoJson",
-      },
-    ],
-    image: {
-      mobile: "/projects/aquaroverbot-mobile.png",
-      desktop: "/projects/aquaroverbot-desktop.png",
-    },
-  },
-  {
-    title: "Durbar",
-    desc: "Durbar is a arduino powered multipurpose robot that can be controlled via bluetooth with a custom application Durbar-Controller",
-    links: [
-      {
-        name: "Github",
-        url: "https://github.com/rudrodip/DurbarDriverProgram",
-      },
-    ],
-    otherLinks: [
-      {
-        name: "C++",
-        image: "/assets/images/techlogo/cpp.svg",
-      },
-      {
-        name: "Arduino",
-        image: "/assets/images/techlogo/arduino.svg",
-      },
-      {
-        name: "PlatformIO",
-      },
-      {
-        name: "ArduinoJson",
-      },
-    ],
-    image: {
-      mobile: "/projects/durbar-mobile.png",
-      desktop: "/projects/durbar-desktop.png",
-    },
-  },
-];
-
-const mobileProjects: item[] = [
-  {
-    title: "AquaRover-controller",
-    desc: "AquaRover-controller is a custom application to control AquaRover robot via bluetooth",
-    links: [
-      {
-        name: "Github",
-        url: "https://github.com/rudrodip/AquaRover-Controller",
-      },
-    ],
-    otherLinks: [
-      {
-        name: "Flutter",
-        image: "/assets/images/techlogo/flutter.svg",
-      },
-      {
-        name: "Dart",
-      },
-      {
-        name: "Firebase",
-        image: "/assets/images/techlogo/firebase.svg",
-      },
-      {
-        name: "Syncfusion",
-      },
-      {
-        name: "Flutter Reactive Ble",
-      },
-    ],
-    image: {
-      mobile: "/projects/aquarover-mobile.png",
-      desktop: "/projects/aquarover-desktop.png",
-    },
-  },
-  {
-    title: "Durbar-controller",
-    desc: "Durbar-controller is a custom application to control Durbar robot via bluetooth",
-    links: [
-      {
-        name: "Github",
-        url: "https://github.com/rudrodip/DurbarController",
-      },
-    ],
-    otherLinks: [
-      {
-        name: "React Native",
-        image: "/assets/images/techlogo/react.svg",
-      },
-      {
-        name: "Javascript",
-        image: "/assets/images/techlogo/js.svg",
-      },
-      {
-        name: "React Native BLE PLX",
-      },
-    ],
-    image: {
-      mobile: "/projects/durbarcontroller-mobile.png",
-      desktop: "/projects/durbarcontroller-desktop.png",
-    },
-  },
-  {
-    title: "CovidCare",
-    desc: "CovidCare is a mobile application that provides real-time information on covid-19 data and the availability of hospital beds, oxygen cylinders, and other medical resources",
-    links: [
-      {
-        name: "Github",
-        url: "https://github.com/rudrodip",
-      },
-    ],
-    otherLinks: [
-      {
-        name: "React Native",
-        image: "/assets/images/techlogo/react.svg",
-      },
-      {
-        name: "Javascript",
-        image: "/assets/images/techlogo/js.svg",
-      },
-    ],
-    image: {
-      mobile: "/projects/covidcare-mobile.png",
-      desktop: "/projects/covidcare-desktop.png",
-    },
-  },
-];
-
-export const featuredProjects: item[] = [
-  webProjects[0],
-  simulationProjects[0],
-  simulationProjects[1],
-  backendProjects[0],
-  roboticsProjects[1],
-  mobileProjects[0],
-  botProjects[0],
-];
-
-export const projects: section<item> = {
-  caption: "Projects",
-  categories: [
-    {
-      name: "Featured",
-      icon: "/categories/featured.png",
-      items: featuredProjects,
-    },
-    {
-      name: "Web",
-      icon: "/categories/web.png",
-      items: webProjects,
-    },
-    {
-      name: "Mobile",
-      icon: "/categories/web.png",
-      items: mobileProjects,
-    },
-    {
-      name: "Backend",
-      icon: "/categories/web.png",
-      items: backendProjects,
-    },
-    {
-      name: "Simulation",
-      icon: "/categories/web.png",
-      items: simulationProjects,
-    },
-    {
-      name: "Robotics",
-      icon: "/categories/web.png",
-      items: roboticsProjects,
-    },
-    {
-      name: "Bot",
-      icon: "/categories/web.png",
-      items: botProjects,
-    },
-  ],
-};
+]
