b32e74714c2977febf8919c6fa4b1414c1cf73d7
rudrodip <official.rudrodipsarker@gmail.com>
2024-04-17 15:13:00 +0600

    Add Display component for console UI

---
+++ src/components/console/display.tsx
@@ -0,0 +1,9 @@
+export const Display = () => {
+  return (
+    <div className="w-full max-w-2xl mx-auto h-full bg-[#5F6A79] rounded-[6px] flex justify-center items-center">
+      <div className="w-[66%] aspect-square bg-[#97C36E] rounded-[4px] flex justify-center items-center">
+        (◕‿↼)
+      </div>
+    </div>
+  )
+}
