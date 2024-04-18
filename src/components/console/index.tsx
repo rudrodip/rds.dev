d623dcea9a38762c97918a644733e176d7a2abaf
rudrodip <official.rudrodipsarker@gmail.com>
2024-04-18 10:56:40 +0600

    Update flex class for centering in console/index.tsx

--- src/components/console/index.tsx
+++ src/components/console/index.tsx
@@ -4,7 +4,7 @@ import { Controller } from "./controller";
 export default function Console() {
   return (
     <div className="relative bg-[#E3DDC5] hidden md:flex flex-col w-[90%] aspect-[1/1.11] border border-[#908683] rounded-[10px] rounded-br-[50px] overflow-hidden">
-      <div className="w-full h-[60%] border-b border-[#908683] flex justify-center items-center pt-6 pb-3 px-10">
+      <div className="w-full h-[60%] border-b br-console flex-cc pt-6 pb-3 px-10">
         <Display />
       </div>
       <div className="w-full h-[40%]">
