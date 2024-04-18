e3cd12efdc0ccdb4f54b3f523ead67307871c8e0
rudrodip <official.rudrodipsarker@gmail.com>
2024-04-18 10:56:49 +0600

    Refactor MiniNavbar component to remove TooltipProvider and Tooltip components

--- src/components/Navbar/MiniNavbar.tsx
+++ src/components/Navbar/MiniNavbar.tsx
@@ -6,12 +6,6 @@ import Link from "next/link";
 import { cn } from "@/lib/utils";
 import { useScroll } from "@/hooks/use-scoll";
 import { Button } from "../ui/button";
-import {
-  Tooltip,
-  TooltipContent,
-  TooltipProvider,
-  TooltipTrigger,
-} from "@/components/ui/tooltip";
 import { motion } from "framer-motion";
 import useMediaQuery from "@src/hooks/use-media-query";
 
@@ -26,7 +20,7 @@ export const MiniNavbar = () => {
         opacity: scrolledEnough ? 1 : 0,
       }}
       className={cn(
-        "flex justify-center sticky top-2 z-[1000] transition-transform transform-gpu duration-200",
+        "flex justify-center sticky top-2 z-[1000] transition-transform transform-gpu duration-200"
       )}
     >
       <div className="bg-transparent navbar-clip">
@@ -35,26 +29,15 @@ export const MiniNavbar = () => {
           <div className="navbar-clip-border"></div>
           <div className="flex justify-between gap-5 items-center px-5 py-1">
             {navbarConfig.miniNav.map((item, index) => (
-              <TooltipProvider key={index} delayDuration={200}>
-                <Tooltip>
-                  <TooltipTrigger asChild>
-                    <Link
-                      href={item.href}
-                    >
-                      <Button
-                        variant="outline"
-                        size="icon"
-                        className="bg-transparent rounded-full border-none"
-                      >
-                        <item.icon className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all" />
-                      </Button>
-                    </Link>
-                  </TooltipTrigger>
-                  <TooltipContent>
-                    <p>{item.href}</p>
-                  </TooltipContent>
-                </Tooltip>
-              </TooltipProvider>
+              <Link href={item.href} key={index}>
+                <Button
+                  variant="outline"
+                  size="icon"
+                  className="bg-transparent rounded-full border-none"
+                >
+                  <item.icon className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all" />
+                </Button>
+              </Link>
             ))}
             <ThemeToggleDropDown />
           </div>
