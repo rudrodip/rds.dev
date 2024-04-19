209ccbf7b00db8f1e8d9e9e9430ac8f278180c19
rudrodip <official.rudrodipsarker@gmail.com>
2024-04-19 09:03:51 +0600

    Update Console component to remove background color

--- src/components/console/index.tsx
+++ src/components/console/index.tsx
@@ -3,7 +3,7 @@ import { Controller } from "./controller";
 
 export default function Console() {
   return (
-    <div className="relative bg-[#E3DDC5] hidden md:flex flex-col w-[90%] aspect-[1/1.11] border border-[#908683] rounded-[10px] rounded-br-[50px] overflow-hidden">
+    <div className="relative hidden md:flex flex-col w-[90%] aspect-[1/1.11] border border-[#908683] rounded-[10px] rounded-br-[50px] overflow-hidden">
       <div className="w-full h-[60%] border-b br-console flex-cc pt-6 pb-3 px-10">
         <Display />
       </div>
