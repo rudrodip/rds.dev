d48f0c38b5c5d7d17645a635273845ec060d4966
rudrodip <official.rudrodipsarker@gmail.com>
2024-04-24 13:33:00 +0600

    Update className in ProjectSection component

--- src/components/Project/project-section.tsx
+++ src/components/Project/project-section.tsx
@@ -2,7 +2,7 @@ import { projects } from "@src/config/projects"
 
 export default function ProjectSection() {
   return (
-    <section className="w-full max-w-4xl mt-7">
+    <section className="w-full max-w-4xl mt-10">
       <h1 className="text-2xl font-bold mb-3 z-50">Featured projects</h1>
       <div className="flex flex-col gap-1">
         {projects.map((project, index) => (
