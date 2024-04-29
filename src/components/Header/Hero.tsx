a8e73610d18a3f84773a1a97e6b8029210def99c
rudrodip <official.rudrodipsarker@gmail.com>
2024-04-29 10:45:30 +0600

    Refactor HeroSection component in Hero.tsx

--- src/components/Header/Hero.tsx
+++ src/components/Header/Hero.tsx
@@ -7,19 +7,10 @@ import { Button } from "@/components/ui/button";
 import Link from "next/link";
 import DiscordApperance from "../Socials/DiscordApperance";
 import { techStack } from "@/config/techstack";
-import { ParallaxText } from "./Parallax";
 import Image from "next/image";
 import { Tech } from "@/types";
-import { hexToRGBA } from "@/lib/utils";
-import { motion } from "framer-motion";
 
 export const HeroSection = () => {
-  const length = techStack.length;
-  const techStack1 = techStack.slice(0, length / 4);
-  const techStack2 = techStack.slice(length / 4, length / 2);
-  const techStack3 = techStack.slice(length / 2, (length / 4) * 3);
-  const techStack4 = techStack.slice((length / 4) * 3, length);
-
   return (
     <section className="w-full flex justify-center items-center my-12 lg:my-24">
       <div className="w-full md:w-1/2 xl:w-full flex flex-col justify-center">
@@ -39,7 +30,7 @@ export const HeroSection = () => {
             </a>
           </span>
         </div>
-        <p className="desc primary-gradient font-semibold">
+        <p className="desc">
           {aboutConfig.bio.substring(0, aboutConfig.bio.lastIndexOf(" "))}{" "}
           <span className="before:block before:absolute before:-inset-1 before:skew-y-3 before:bg-blue-400 before:dark:bg-blue-500 relative inline-block">
             <span className="relative text-white">
@@ -49,73 +40,23 @@ export const HeroSection = () => {
         </p>
         <div className="flex gap-4 align-middle flex-wrap my-5">
           <Button asChild variant="outline" size="sm">
-            <Link href="/#contact">Get in touch</Link>
+            <Link href="https://cal.com/rds_agi">Get in touch</Link>
+          </Button>
+          <Button asChild variant="outline" size="sm">
+            <Link href="https://raw.githubusercontent.com/rudrodip/rudrodip/main/resume.pdf" className="relative">
+              Resume
+              <span className="w-[8px] h-[8px] rounded-full absolute top-[-3px] right-[-3px] animate-ping bg-primary"></span>
+            </Link>
           </Button>
           <SocialIcons />
         </div>
         <ExperienceSection />
       </div>
       <div className="hidden relative xl:flex flex-col h-[600px] w-full justify-center items-end">
-        <div
-          className="overflow-hidden w-auto h-[600px] flex gap-10 justify-end items-center  
-          after:content['']
-          after:to-transparent
-          after:from-background
-          after:bg-gradient-to-t
-          after:right-0
-          after:bottom-0
-          after:w-[70%]
-          after:h-20
-          after:z-10
-          after:absolute
-
-          before:content['']
-          before:to-transparent
-          before:from-background
-          before:bg-gradient-to-b
-          before:top-0
-          before:right-0
-          before:w-[70%]
-          before:h-20
-          before:z-10
-          before:absolute"
-        >
-          <div>
-            <ParallaxText baseVelocity={-1}>
-              <div className="flex flex-col gap-2">
-                {techStack1.map((tech) => (
-                  <TechLogo key={tech.name} tech={tech} />
-                ))}
-              </div>
-            </ParallaxText>
-          </div>
-          <div>
-            <ParallaxText baseVelocity={1}>
-              <div className="flex flex-col gap-2">
-                {techStack2.map((tech) => (
-                  <TechLogo key={tech.name} tech={tech} />
-                ))}
-              </div>
-            </ParallaxText>
-          </div>
-          <div>
-            <ParallaxText baseVelocity={-1}>
-              <div className="flex flex-col gap-2">
-                {techStack3.map((tech) => (
-                  <TechLogo key={tech.name} tech={tech} />
-                ))}
-              </div>
-            </ParallaxText>
-          </div>
-          <div>
-            <ParallaxText baseVelocity={1}>
-              <div className="flex flex-col gap-2">
-                {techStack4.map((tech) => (
-                  <TechLogo key={tech.name} tech={tech} />
-                ))}
-              </div>
-            </ParallaxText>
-          </div>
+        <div className="w-[90%] grid grid-cols-5 gap-3 place-items-center bg-sky-300/10 p-3 rounded-xl">
+          {techStack.map((tech, index) => (
+            <TechLogo key={index} tech={tech} />
+          ))}
         </div>
       </div>
     </section>
@@ -123,17 +64,12 @@ export const HeroSection = () => {
 };
 
 export const TechLogo = ({ tech }: { tech: Tech }) => {
-  const experiencePercentage = tech.experience * 100;
-  const background = `linear-gradient(0deg, ${hexToRGBA(
-    tech.color,
-    0.5
-  )} ${experiencePercentage}%, rgba(211, 211, 211, 0.5) ${experiencePercentage}%)`;
   return (
-    <motion.div
-      className="w-full aspect-square p-3 border rounded items-center justify-center flex btn-gradient"
-      whileHover={{ background: background }}
+    <div
+      className="w-full aspect-square p-3 items-center justify-center flex flex-col rounded-lg border group border-transparent hover:border-border hover:scale-110 hover:bg-cyan-400/30 transition-all transform-gpu duration-75"
     >
       <Image src={tech.icon} alt={tech.name} width={50} height={50} />
-    </motion.div>
+      <span className="text-xs invisible group-hover:visible absolute text-center bottom-0">{tech.name}</span>
+    </div>
   );
 };
